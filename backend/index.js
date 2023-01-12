88efa416dd10dcb198880f1ce94262d400a80a6e
unknown <3.ibrahim2000@gmail.com>
2023-01-12 15:52:11 +0200

    Mern Stack E-Commerce

--- backend/index.js
+++ backend/index.js
@@ -25,6 +25,10 @@ app.use('/api/carts',cartRouter);
 app.use('/api/orders',orderRouter);
 app.use('/api/stripe',stripeRouter);
 
+app.use('/api',(req,res)=>{
+    res.send('APP IS RUNNING !');
+})
+
 app.listen(process.env.PORT || 5000);
 mongoose.set('strictQuery', true);
 mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true,useUnifiedTopology:true})
